2f4523628989d6b6b2a3a2ec8935098fdc8d4c5d
Mohammad Banikazemi <mb@us.ibm.com>
2019-10-16 16:12:20 -0400

    simple manipulations

--- controllers/servicebinding_controller.go
+++ controllers/servicebinding_controller.go
@@ -17,6 +17,7 @@ package controllers
 
 import (
 	"context"
+	"strings"
 
 	"istio.io/pkg/log"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -46,7 +47,33 @@ func (r *ServiceBindingReconciler) Reconcile(req ctrl.Request) (ctrl.Result, err
 		return ctrl.Result{}, ignoreNotFound(err)
 	}
 
-	log.Warnf("processed SB resource: %v", binding)
+	mfcSelector := binding.Spec.MeshFedConfigSelector
+	s := strings.Split(mfcSelector, "=")
+	var mfcList mmv1.MeshFedConfigList
+	var mfc mmv1.MeshFedConfig
+
+	if mfcSelector == "" {
+		log.Infof("No configs selector for SB '%v'. using default Selector.", binding.Name)
+		// TODO: use Default config
+	} else {
+		if len(s) == 2 {
+			if err := r.List(ctx, &mfcList, client.MatchingLabels{s[0]: s[1]}); err != nil {
+				log.Warnf("Unable to fetch SB. Error: %v", err)
+				return ctrl.Result{}, err
+			}
+			if len(mfcList.Items) == 1 {
+				mfc = mfcList.Items[0]
+				log.Infof("Processing SB '%v' and found MeshFedConfig: '%v' ", binding.Name, mfc.Name)
+			} else {
+				log.Warnf("Mulitple configs selected for SB '%v'. selector: %v", binding.Name, mfcSelector)
+				// TODO: return error
+			}
+		} else {
+			// TODO: return error
+			log.Warnf("Bad MeshFedConfig selector for SB '%v'", binding.Name)
+		}
+	}
+
 	return ctrl.Result{}, nil
 }
 
